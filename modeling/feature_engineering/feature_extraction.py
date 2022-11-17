dc81d221512f66250d29cee7d720c814da6f4556
unknown <23522048@std.stei.itb.ac.id>
2022-11-18 00:29:15 +0700

    New experiment on XGBReg, new extracted features

---
+++ modeling/feature_engineering/feature_extraction.py
@@ -0,0 +1,36 @@
+import talib
+
+def generate_TAs_features(df):
+    """
+    Function to generate Technical Analysis features:
+    - MA (Moving Average)
+    - RSI (Relative Strength Index)
+    - MFI (Money Flow Index)
+    """
+    res = df.copy()
+    for i in [7, 14, 21]:
+        res[f'{i} DAYS MA'] = talib.MA(res['Close'], timeperiod=i)
+        res[f'RSI {i}'] = talib.RSI(res['Close'], timeperiod=i)
+        res[f'MFI {i}'] = talib.MFI(res['High'], res['Low'], 
+                                res['Close'], res['Volume'], 
+                                timeperiod=i)
+        if i == 7:
+            res[f'{i} DAYS STD DEV'] = res['Close'].rolling(i).std()
+
+    res = res.dropna()
+    res['Close'] = res['Close'].shift(-1)
+    return res
+
+def generate_date_related_features(df):
+    """
+    Function to generate date related features
+    """
+    res = df.copy()
+    res['dayofweek'] = df.index.dayofweek
+    res['quarter'] = df.index.quarter
+    res['month'] = df.index.month
+    res['year'] = df.index.year
+    res['dayofyear'] = df.index.dayofyear
+    res['dayofmonth'] = df.index.day
+    res['weekofyear'] = df.index.isocalendar().week
+    return res
